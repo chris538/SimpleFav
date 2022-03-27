d22f4445ddb7233dc0b8d6c7f7b6a6da2c026ad3
Chris Le <giants_broncos75@yahoo.com>
2022-03-27 00:42:25 -0500

    Updated Controllers

--- SimpleFav/ViewController.swift
+++ SimpleFav/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    //var videoArray = [NSDictionary]()
+    
+    //let myRefreshControl = UIRefreshControl()
+    
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //tableView.dataSource = self
+        //tableView.delegate = self
+        
+        //myRefreshControl.addTarget(Self, action: #selector(), for: T##UIControl.Event)
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "playlistCell", for: indexPath) as! PlaylistCellTableViewCell
+        
+        cell.usernameLabel.text = "Some name"
+        cell.videoTitleLabel.text = "Some title"
+        cell.videoDescriptionLabel.text = "Some description"
+        
+        //cell.textLabel?.text = "row: \(indexPath.row)"
+        
+        return cell
+    }
 
 }
-
