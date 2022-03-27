e27d46c6fe6bfbc26d9bf5af80cad8aa78ea015e
Chris Le <giants_broncos75@yahoo.com>
2022-03-26 20:47:32 -0500

    Initial Commit

---
+++ SimpleFav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleFav
+//
+//  Created by Chris Le on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
